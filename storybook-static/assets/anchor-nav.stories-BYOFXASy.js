f80f20fd8fb634ad8ce27c80e53094d4cde517ef
Abel Munoz <amunoz@mightycitizen.com>
2025-04-24 16:32:30 -0700

    batch 2 qa fixes

--- storybook-static/assets/anchor-nav.stories-BYOFXASy.js
+++
@@ -1,5 +0,0 @@
-import{t as n}from"./anchor-nav-Df2wWrWY.js";import{a as s}from"./global-DPFDjWLF.js";import"./twig-BXd3Gfe2.js";import"./_commonjsHelpers-BosuxZz1.js";import"./twig-foVUGi9y.js";const g={title:"Components/Navigation/Anchor Nav",argTypes:{}},i=o=>n(o),r=i.bind({});r.args={anchor_nav:s};var a,t,e;r.parameters={...r.parameters,docs:{...(a=r.parameters)==null?void 0:a.docs,source:{originalSource:`args => {
-  // You can either use a function to create DOM elements or use a plain html string!
-  // return \`<div>\${label}</div>\`;
-  return twig(args);
-}`,...(e=(t=r.parameters)==null?void 0:t.docs)==null?void 0:e.source}}};const l=["AnchorNav"];export{r as AnchorNav,l as __namedExportsOrder,g as default};
---
+++ storybook-static/assets/footer.stories-Dc8NLRnc.js
@@ -0,0 +1,5 @@
+import{t as a}from"./footer-NQ1pCZuR.js";import{c as i,d as n,e as m,l as p,f as c,s as u,m as l}from"./global-C96tF7Vx.js";import"./twig-BXd3Gfe2.js";import"./_commonjsHelpers-BosuxZz1.js";import"./twig-foVUGi9y.js";import"./social-media-BBQJ4pE2.js";import"./logo-CbkVP_fk.js";import"./back-to-top-Bf3x7Log.js";import"./link-CIbey7wG.js";const O={title:"Layout/Wrapper/Footer"},d=s=>a(s),r=d.bind({});r.args={menu:l,social:u,footer_links:c,logo:p,address:m,menu_column:n,site_name:i};var e,o,t;r.parameters={...r.parameters,docs:{...(e=r.parameters)==null?void 0:e.docs,source:{originalSource:`args => {
+  // You can either use a function to create DOM elements or use a plain html string!
+  // return \`<div>\${label}</div>\`;
+  return twig(args);
+}`,...(t=(o=r.parameters)==null?void 0:o.docs)==null?void 0:t.source}}};const k=["Footer"];export{r as Footer,k as __namedExportsOrder,O as default};
