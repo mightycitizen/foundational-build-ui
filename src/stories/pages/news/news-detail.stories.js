2d954ac7ea53ab8174f5b524c442f69dbaf297b3
Abel Munoz <amunoz@mightycitizen.com>
2025-07-18 16:33:57 -0700

    news detail in progress

--- src/stories/pages/news/news-detail.stories.js
+++ src/stories/pages/news/news-detail.stories.js
@@ -1,12 +1,22 @@
 import twig from './news-detail.twig';
-import { menu, social, logo } from '../../global/placeholders/global.json';
+import { menu, social, logo, address, utility_menu, cta_menu, header_dropdown, menu_column, anchor_nav, local_nav, links_bar_menu, footer_links, site_name } from '../../global/placeholders/global.json';
 
 import { placeholder, sizes } from '../../global/placeholders/images.json';
 
 import {
+  accordion,
+  tabs,
+  breadcrumbs,
+  hero_primary,
+  hero_secondary,
+  card,
+  contact,
+  testimonial,
   wysiwyg,
-  pagination,
-  breadcrumbs
+  video,
+  steps,
+  statistic,
+  links
 } from '../../global/placeholders/components.json';
 //More on default export: https://storybook.js.org/docs/html/writing-stories/introduction#default-export
 export default {
@@ -21,14 +31,108 @@ const Template = (args) => {
 };
 
 const defaultArgs = {
-  logo,
   breadcrumbs,
   menu,
+  utility_menu,
+  links_bar_menu,
+  anchor_nav,
+  local_nav,
+  cta_menu,
+  header_dropdown,
+  logo,
   social,
-  image_sizes: sizes,
-  image_placeholder: placeholder,
-  wysiwyg,
-  pagination
+  address,
+  footer_links,
+  site_name,
+  menu_column,
+  page_title: 'Article title / headline goes here - lorem ipsum dolor sit amet',
+  page_subheading: '',
+  page_date: 'May 9, 2025',
+  page_author: 'Firstname Lastname',
+  page_author_url: '#',
+  page_image: {
+    src: '',
+    alt: ''
+  },
+  page_sidebar: {
+    topics: [
+      {
+        text: 'Alumni',
+        url: '#'
+      },
+      {
+        text: 'Topic name here',
+        url: '#'
+      },
+      {
+        text: 'Topic name here',
+        url: '#'
+      },
+      {
+        text: 'Topic name here',
+        url: '#'
+      },
+    ],
+    publication: {
+      text: 'Orange Cape Newsletter',
+      url: 'Orange Cape Newsletter'
+    },
+    authors: [
+      {
+        thumbnail: {
+          src: '/images/author-thumb.jpg',
+          alt: 'author'
+        },
+        name: 'Authorname Lastname',
+        url: '#',
+        org_name: 'Organization name',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras dapibus vulputate diam eu pretium. Mauris elit orci, ultricies id fermentum vel, porta et eros... '
+      },
+      {
+        thumbnail: {
+          src: '/images/author-thumb.jpg',
+          alt: 'author'
+        },
+        name: 'Authorname Lastname',
+        url: '#',
+        org_name: 'Organization name',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras dapibus vulputate diam eu pretium. Mauris elit orci, ultricies id fermentum vel, porta et eros... '
+      }
+    ],
+    media_name: 'Firstname Lastname',
+    media_email: 'email@mightyu.edu',
+    media_phone: '512-555-1234'
+  },
+  narrow_modules: [
+    {
+      module_type: 'wysiwyg',
+      wysiwyg
+    },
+    {
+      module_type: 'accordion',
+      exclude_container: true,
+      section_id: 'section-2',
+      ...accordion
+    }
+  ],
+  sidebar_modules: [
+    {
+      module_type: 'card',
+      ...card
+    }
+  ],
+  modules: [
+    {
+      module_type: 'cta',
+      section_heading: 'Default CTA',
+      section_description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      section_button: {
+        url: '#',
+        text: 'Text'
+      }
+    }
+  ]
 };
 const parameters = {
   layout: 'fullscreen'
@@ -38,14 +142,4 @@ const Default = Template.bind({});
 Default.args = defaultArgs;
 Default.parameters = parameters;
 
-const FeaturedImage = Template.bind({});
-FeaturedImage.args = {
-  ...defaultArgs,
-  featured_image: {
-    src: 'https://placehold.co/430x230',
-    alt: ''
-  }
-};
-FeaturedImage.parameters = parameters;
-
-export { Default, FeaturedImage };
+export { Default };
